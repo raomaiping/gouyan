6cce1f43f6f76112e68d3d553ef994b855e858b7
raomaiping <2582395486@qq.com>
2019-09-08 17:22:51 +0800

    搜索

--- my-app/src/pages/movie/search/Search.js
+++ my-app/src/pages/movie/search/Search.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
-
+import AppHeader from "../../../components/app-header"
 export default class Search extends Component {
     render() {
+        let leftBtn = <span className='header-left-btn iconfont icon-fanhui' onClick={this.backAction}></span>;
         return (
             <div className='page subpage'>
-                <h1>搜索</h1>
+                <AppHeader leftBtn={leftBtn} title='猫眼电影' />
+                <div className='content'>
+                    <h1>搜索</h1>
+                </div>
             </div>
         )
+    }
+    backAction = ()=>{
+        this.props.history.goBack();
     }
 }
